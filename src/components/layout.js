4d93812f226d98f4b0315f609b71834b63189682
Vann-Dogg <vann.dogg.93@gmail.com>
2020-05-31 21:33:49 +0300

    SiteTitle

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
+import Seo from 'seo'
 import Header from "./header"
 import Nav from "./nav"
 import "../styles/fonts.css"
@@ -20,6 +20,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <Seo title="Слушай. Молись. Размышляй." />
       <Header siteTitle={data.site.siteMetadata.title} />
       <main id="content">{children}</main>
       <Nav />
